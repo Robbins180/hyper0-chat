fcf1222b702ec7a7edeecc34122e9b56b64d1c80
Robbins180 <aRobbins180@gmail.com>
2021-09-09 16:28:45 -0500

    chatapp

---
+++ src/learned.js
@@ -0,0 +1,13 @@
+ {/* How to fix firebase 9.0 import error
+https://stackoverflow.com/questions/68946446/how-do-i-fix-a-firebase-9-0-import-error-attempted-import-error-firebase-app
+
+
+*/}
+
+apiKey: "AIzaSyA_rdPR2ll0-detkxRaGkP8pn2r4gPrP7o",
+authDomain: "hyper0-chat.firebaseapp.com",
+projectId: "hyper0-chat",
+storageBucket: "hyper0-chat.appspot.com",
+messagingSenderId: "651522701406",
+appId: "1:651522701406:web:17110ad6cc28deae7a5ccf",
+measurementId: "G-VX82WPM0D9"
